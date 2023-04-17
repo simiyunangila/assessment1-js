919f4981c49df84300e18c7a084359ea7c468943
simiyunangila <124134210+simiyunangila@users.noreply.github.com>
2023-04-17 15:22:29 +0300

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -1,65 +1,112 @@
 // 1. Write a function that takes in a string and returns it when reversed
 // let food = “eating”
 function x(food){
+    console.log(food.split("").reverse().join(""))
 
 }
 let food=`eating`
-console.log(x(food))
+x(food)
 // 2. Write a function that takes in the following array and consoles the target if it is found else
 // null
 // let num = [2,8,0,23,5,45,76]
 // Target = 23
-function arraynums(){
-
+function arraynums(numbs){
+numbs.length<=1{
+    return numbs
 }
+    let mid=Math.floor(numbs.length/2);
+    let left= numbs.slice(0,mid);
+    let right=numbs.slice(mid);
+    return sortedArray(arraynums(left),arraynums(right));
+}
+function sortedArray(left,right){
+emptyarray=[];
+    while(left.length && right.length){
+        if(left[0] < right[0]);{
+        emptyArray.push(left.shift());
+        }
+        else{
+        emptyArray.push(right.shift())
+        }
+    }
+    return[...emptyArray,...left,...right]
+}
+function binarySearch(num,target){
+    let left=0;
+    let right=num.length -1;
+    when(left<=right){
+    let middle= Math.floor(left+right/2)
+    if(num[middle]===target){
+        return middle;
+    }
+        else if(num[middle]>target){
+        right= middle -1;
+        }
+        else{
+        left=middle+1;
+        }
+    }
+    return null;
+}
+let target=23;
+let num=arraynums([2,8,0,23,5,45,76]);
+console.log(arraynums(numbs));
+console.log(binarySearch(num,target));
 
 
 // 3. Given years between 2000 and 2023, console all the leap years in the following
 // sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
-function leapyear(y){
-    for (w=[];w<y.length;w++){
-
+function leapyear(){
+    for (let year=2000;year=2023;year++){
+        if(year % 4 === 0){
+            console.log(year +"is a leap year")
+        }
+        else{
+            console.log(year + "is not a leap year")
+        }
     }
-
 }
+leapyear()
 
 
 // Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
 // “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
-function range (numbers){
-    for(x=0;x<numbers.length;x++){
-       if(numbers.length/3==0 && numbers.length/5==0) {
-        return[`FizzBuzz`]
-       }else if(numbers.length/3==0){
-        return[`Fizz`]
-       }else{
-        return[`Buzz`]
+function range (){
+    for(let x=0;x=100;x++){
+       if(x % 3 == 0 && x % 5 === 0) {
+        console.log("FizzBuzz")
+       }
+        else if(x % 3 === 0){
+        console.log("Fizz")
+       }
+        else(x % 5 === 0){
+        console.log("Buzz")
        }
     }
 }
-let numbers=[]
-console.log(range(numbers))
+range()
 
 
 // 5. Write a function that takes in an array of numbers and returns an array that has all
 // elements multiplied by 4.
 // let numArray = [12,87,45,75,23,64,73]
 function arrays(numArray){
-    for(z=[];z<numArray.length;z++){
-        z.push(numArray*4)
-    }
+   numarray.map(function(y))
+   console.log(y*4)
 }
 let numArray = [12,87,45,75,23,64,73]
-console.log(arrays(numArray))
+arrays(numArray)
 
 // 6. Write a function that takes in an array of strings and returns an array with every element
 // turned into a number
 // let nums = [“10”,”24”,”45”,”56”,”67”]
 
 function numbersarray (nums){
-for (newarr=[];newarr<nums.length;newarr++){
-    newarr.push(nums*1)
-}
+    let r=[]
+numbersarray.map(function(number){
+r.push({"number":Number(number)})
+})
+console.log(r)
 }
 let nums=[`10`,`24`,`45`,`56`,`67`]
-console.log(numbersarray(nums))
+numbersarray(nums)
